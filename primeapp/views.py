0468e6e59b67aba5cbe8ddb0abe4024996b6a7af
Mohd.Irshad <mohdirshadmi4@gmail.com>
2016-05-14 11:43:11 +0530

    created form

--- primeapp/views.py
+++ primeapp/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.views.generic import View
+from .forms import PrimeForm
+
 
 class MyView(View):
+	template_name = 'form_template.html'
+	form_class = PrimeForm
     def get(self, request):
-        # <view logic>
+        # <view logic
         return HttpResponse('result')
+
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():            
+            return HttpResponseRedirect('/success/')
+
+        return render(request, self.template_name, {'form': form})
